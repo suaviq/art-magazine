1d14060b655cd30e23800e8a99e0d511116cebec
suaviq <alasen24@gmail.com>
2020-10-13 21:25:57 +0200

    first commit

---
+++ ascii.cpp
@@ -0,0 +1,20 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	char c1, c2, c3, c4, c5;
+//	cout << "Enter 5 letters: ";
+//	cin >> c1 >> c2 >> c3 >> c4 >> c5;
+//	cout << "ASCII message: " << int(c1) << " " << int(c2) << " "
+//		<< int(c3) << " " << int(c4) << " " << int(c5) << endl;
+//
+//
+//	int i1, i2, i3, i4, i5;
+//	cout << "Enter 5 numbers from 0 to 128: ";
+//	cin >> i1 >> i2 >> i3 >> i4 >> i5;
+//	cout << "In ASCII it means: " << char(i1) << char(i2) << char(i3) 
+//		<< char(i4) << char(i5);
+//
+//	system("pause>0");
+//}
---
+++ minmax.cpp
@@ -0,0 +1,27 @@
+﻿//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int array[6] = { 1, 3, 5, 7, 8, 11 };
+//	int n = 6;
+//	int max_val = array[0];
+//	for (int i = 1; i < n; i++) {
+//		if (array[i] > max_val) {
+//			max_val = array[i];
+//		}
+//	}
+//	cout << "Max value is " << max_val << endl;
+//
+//	int min_val = array[0];
+//	for (int i = 1; i < n; i++) {
+//		if (array[i] < min_val) {
+//			min_val = array[i];
+//		}
+//	}
+//	cout << "Min value is " << min_val << endl;
+//
+//	system("pause>0");
+//
+//
+//}
