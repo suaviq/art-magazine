568641f7d682fe18764a8ea976624df9fdd75536
suaviq <alasen24@gmail.com>
2020-10-13 22:48:08 +0200

    second command

---
+++ sort.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int number[7];
+	int i, j;
+	for (i = 0; i <= 6; i++) {
+		cout << "Please enter number: ";
+		cin >> number[i];
+	}
+	for (i = 0; i <= 5; i++) {
+		for (j = i + 1; j <= 6; j++) {
+			int temp;
+			if (number[i] > number[j]) {
+				temp = number[i];
+				number[i] = number[j];
+				number[j] = temp;
+			}
+		}
+	}
+	for (i = 0; i <= 6; i++) {
+		cout << endl <<  number[i] << endl;
+	}
+	system("pause>0");
+}
